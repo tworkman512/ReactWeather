259156c9f782658d3cda568d62331516be3de840
tom-workman <tom.workman@us.ibm.com>
2016-08-13 10:04:19 -0500

    Fixed copy

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ var express = require('express');
 
 // Create our app
 var app = express();
-const PORT = process.evn.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(function(req, res, next) {
-  if (req.header['x-forwarded-proto'] === 'http') {
+  if (req.headers['x-forwarded-proto'] === 'http') {
     next();
   } else {
     res.redirect('http://' + req.hostname + req.url);
